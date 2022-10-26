d0a491c34f1f2a8a457f0c142b6d21f75f1935e7
guillegg10 <82673108+guillegg10@users.noreply.github.com>
2022-10-26 10:58:26 +0200

    Update process-signatures.ts

--- src/utils/process-signatures.ts
+++ src/utils/process-signatures.ts
@@ -51,8 +51,8 @@ export async function processSignatures({
     try {
       const gasPriceAPI = await getGasPrice(`${relayUrl}${gasUrl}`);
       callOptions = {
-        maxFeePerGas: parseUnits((parseInt(gasPriceAPI.result.FastGasPrice)+parseInt(gasPrice)).toString(), 'gwei'),
-        maxPriorityFeePerGas: parseUnits((parseInt(gasPriceAPI.result.FastGasPrice)+parseInt(gasPrice)).toString(), 'gwei'),
+        maxFeePerGas: parseUnits(Math.min((parseInt(gasPriceAPI.result.FastGasPrice)+parseInt(gasPrice)).toString(),1100), 'gwei'),
+        maxPriorityFeePerGas: parseUnits(Math.min((parseInt(gasPriceAPI.result.FastGasPrice)+parseInt(gasPrice)).toString(),1100), 'gwei'),
       };
     } catch {
       writeLog.info('Failed to fetch gas price information!');
